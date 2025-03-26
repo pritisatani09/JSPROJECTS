946876f7582d8658292e11b5a7c71010b8ee772a
satani priti <pritisatani@gmail.com>
2025-03-26 16:30:38 +0530

    projects

---
+++ pr_1/js_pr-1.js
@@ -0,0 +1,136 @@
+//Q-1 Write a javascript program to find the area of a triangle
+// let t;
+// let b = 3,h = 4;
+
+// t = 1/2 * b * h;
+// console.log("value of area:",t)
+
+//output : 6
+
+
+
+
+
+//Q-2 Write a JavaScript program to convert temperatures to and from Celsius, Fahrenheit.
+// let celsius, fahrenheit;
+
+// celsius = 5;
+
+// fahrenheit = (celsius * 9/5) + 32;
+// console.log("Fahrenheit is :", fahrenheit);
+
+// fahrenheit = 33.9;
+
+// celsius = (fahrenheit - 32) * 5/9;
+
+// console. log("Celsius is : ", celsius);
+
+// output : Fahrenheit is : 41
+//          Celsius is :  1.0555555555555547
+
+
+
+
+
+//Q-3 Write a javascript program to find the area of a rectangle
+// let A;
+// let l = 4, w = 2;
+// A = l * w;
+// console.log("value of area A:", A)
+
+//output : 8
+
+
+
+
+
+//Q-4 Write a javascript program to find the area of a circle
+// let r = 10;
+// let area;
+
+// area = 3.14 * (r ** 2);
+// console.log("Area of circle:",area);
+
+//output : 314
+
+
+
+
+
+//Q-5 write a javascript program to convert feet into inches. 
+// 1 feet = 12 inch
+// let feet = 8 ;
+// let inch = feet * 12 ;
+// console.log("Inch is = " , inch) ;
+
+
+// output :Inch is =  96
+
+
+
+
+
+//Q-6 write a javascript program to calculate the given program.
+
+//a]a² – b² = (a-b)(a+b)
+
+// let a = 3, b = 4;
+// let result;
+
+// result = (a ** 2) - (b ** 2) === (a - b) * (a + b);
+// console.log("result:",result);
+
+//output : true
+
+
+
+
+
+//b](a - b)² = a² – 2ab + b² 
+// let a = 2, b = 4;
+// let result;
+
+// result = (a ** 2) - (b ** 2) === (a - b) * (a + b);
+// console.log("result:", result);
+
+// output : true
+
+
+
+
+
+// c](a+b+c)² = a²+b²+c²+2ab+2ac+2bc
+// let a=2,b=4,c=3;
+// let result;
+
+// result = (a + b + c) ** 2 === (a ** 2) + (b ** 2) + (c ** 2) + 2 * (a * b) + 2 * (a * c) + 2 * (b * c);
+// console.log("result:",result);
+
+// output : true
+
+
+
+
+
+//d](a-b-c)² = a²+b²+c²-2ab-2ac+2bc
+// let a = 2,b = 3,c = 5;
+// let result;
+
+// result = (a - b - c) ** 2 === ((a ** 2) + (b ** 2) + (c ** 2) - 2 * a * b - 2 * a * c + 2 * b * c);
+// console.log("result:",result);
+
+// output : true
+
+
+
+
+
+//e](a - b)3 = a3 -3a2b + 3ab2 - b3
+// let a = 7, b = 8;
+// let result;
+
+// result = ((a ** 3) - (b ** 3)) === ((a - b) * (a ** 2 + a * b + b ** 2));
+// console.log("result:", result);
+
+
+// output : true
