12bd2ecacca69790961b6b90d80dbadefdb67033
satani priti <pritisatani@gmail.com>
2025-04-08 18:25:40 +0530

    8-04-25

---
+++ pr_4/function.js
@@ -0,0 +1,110 @@
+// 1. Write a JAVASCRIPT Program to print a greeting message using with argument no return type function when call a function without argument at that time print message Good Morning.
+// function greet(message = "Good Morning") {
+//     console.log(message);
+//   }
+// greet("Welcome to JavaScript!");
+// greet();  
+
+// Output:Welcome to JavaScript!
+//        Good Morning 
+  
+
+// 2. Write a JAVASCRIPT Program to find Circle area (area = pi*r*r) using return type with argument function.
+// function circleArea(radius) {
+//     let area = 3.14 * radius * radius;
+//     return area;
+//     }
+//     console.log(circleArea(15)); 
+
+// Output: 706.5
+
+
+// 3. Write a JAVASCRIPT Program to find Triangle area ( area = (l*h)/2 ) using return type with argument function.
+// function triangleArea(base, height) {
+//     let area = (base * height) / 2;
+//     return area;
+//     }
+//     console.log(triangleArea(13, 8));
+
+// Output ==> 52
+
+
+// 4. Write a JAVASCRIPT Program to find Rectangle area = I * h using return type with argument function.
+// function rectangleArea(length, height) {
+//     let area = length * height;
+//     return area;
+//     }
+//     console.log(rectangleArea(14, 14));
+
+// Output ==>196
+
+
+// 5. Write a JAVASCRIPT Program to find ans of ((b*b)(4*a*c))/(2*a) using return type with argument function.
+// function ans(a, b, c) {
+//     let ans = ((b * b) * (4 * a * c)) / (2 * a);
+//     return ans;
+//     }
+//     console.log(ans(3, 6, 9));  
+
+// Output ==> 648
+
+
+// 6. Write a JAVASCRIPT Program to find ans of (a*a) + (2*a*b) + (b*b) using return type with argument function
+// function ans(a, b) {
+//     let ans = (a * a) + (2 * a * b) + (b * b);
+//         return ans;
+//         }
+//         console.log(ans(8, 5)); 
+
+// Output ==>169
+
+
+// 7. Write a JAVASCRIPT Program to convert Fahrenheit to Celsius (c = (f-32)/1.8 ) using return type with argument function
+// function fahrenheitToCelsius(fahrenheit) {
+//     let celsius = (fahrenheit - 32) / 1.8;
+//     return celsius;
+//     }
+//     console.log(fahrenheitToCelsius(78)); 
+
+// Output ==>25.5555
+
+
+// 8. Write a JAVASCRIPT Program to convert Celsius to Fahrenheit (f= (c*1.8)+32 ) using return type with argument function
+// function celsiusToFahrenheit(celsius) {
+//     let fahrenheit = (celsius * 1.8) + 32;
+//     return fahrenheit;
+//     }
+//     console.log(celsiusToFahrenheit(3));
+
+// Output ==>37.4
+
+
+// 9. Write a JAVASCRIPT Program to find if a given number is odd or even using anargument with no return type.
+// function checknumber(num) {
+//     if (num % 2 == 0) {
+//         console.log("Number is even:",num);
+//         }
+//         else {
+//             console.log("Number is odd:",num);
+//             }
+//             }
+//             checknumber(33);
+
+// Output ==>Number is odd: 33
+
+
+// 10. Write a JAVASCRIPT Program to swap a value without third variable using with argument no return type.
+// function swapValues(a, b) {
+//     a = a + b;
+//     b = a - b;
+//     a = a - b;
+//     console.log("Value of a is " , a);
+//     console.log("Value of b is " , b);
+//     }
+//     swapValues(24,12);
+
+// Output ==>Value of a is  12
+//           Value of b is  24
+
+
+
